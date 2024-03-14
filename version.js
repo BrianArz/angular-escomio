4d7bc8ea6c3264ad530da04c7f907f31c4d0bf5c
Brian Arizmendi <arz.dev@outlook.com>
2024-03-14 16:20:40 -0600

    [VersionFile] - docker validation

--- version.js
+++ version.js
@@ -2,24 +2,19 @@
 // Modified by ArzDev: https://github.com/BrianArz
 
 const fs = require("fs");
+const execSync = require("child_process").execSync;
 
-// If its running on docker container do not execute
-if(!fs.existsSync('./.skip_version_file'))
-{
-    const execSync = require("child_process").execSync;
+const gitVersion = execSync("git rev-list --count --all").toString().trim();
+const commitHash = execSync("git rev-parse HEAD").toString().trim().substring(0,6);
+const buildDate = new Date().toISOString();
 
-    const gitVersion = execSync("git rev-list --count --all").toString().trim();
-    const commitHash = execSync("git rev-parse HEAD").toString().trim().substring(0,6);
-    const buildDate = new Date().toISOString();
+const content = `
+export const version = '1.0.${gitVersion}';
+export const buildDate = '${buildDate}';
+export const commitHash = '${commitHash}';
+`;
 
-    const content = `
-    export const version = '1.0.${gitVersion}';
-    export const buildDate = '${buildDate}';
-    export const commitHash = '${commitHash}';
-    `;
-
-    fs.writeFileSync("./src/environments/version.prod.ts", content);
-}
+fs.writeFileSync("./src/environments/version.prod.ts", content);
 
 
 
