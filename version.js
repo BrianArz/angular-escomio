061d11795227e5947e010ab165fe1380a6e0e9db
Arz Dev <arz.dev@outlook.com>
2024-03-14 19:35:47 -0600

    [VersionFile] + isDIrty

--- version.js
+++ version.js
@@ -5,11 +5,13 @@ const fs = require("fs");
 const execSync = require("child_process").execSync;
 
 const gitVersion = execSync("git rev-list --count --all").toString().trim();
+const gitDiff = execSync("git diff --name-only HEAD").toString().trim();
+const isDirty = gitDiff ? '1' : '0';
 const commitHash = execSync("git rev-parse HEAD").toString().trim().substring(0,6);
 const buildDate = new Date().toISOString();
 
 const content = `
-export const version = '1.0.${gitVersion}';
+export const version = '1.0.${gitVersion}.${isDirty}';
 export const buildDate = '${buildDate}';
 export const commitHash = '${commitHash}';
 `;
