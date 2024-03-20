72490c33a966b386745662ee2a10b746160f6911
Arz Dev <arz.dev@outlook.com>
2024-03-20 00:16:25 -0600

    [VersonFile] + gitVersion fix

--- version.js
+++ version.js
@@ -4,7 +4,7 @@
 const fs = require("fs");
 const execSync = require("child_process").execSync;
 
-const gitVersion = execSync("git rev-list --count --all").toString().trim();
+const gitVersion = execSync("git rev-list --count HEAD").toString().trim();
 const gitDiff = execSync("git diff --name-only HEAD").toString().trim();
 const isDirty = gitDiff ? '1' : '0';
 const commitHash = execSync("git rev-parse HEAD").toString().trim().substring(0,6);
