d06e8109501fb2b01c3c22ba5ee108c1663e5856
Arz Dev <arz.dev@outlook.com>
2024-06-01 18:31:24 -0600

    [components/navbar] + small logged logic

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 
+import { AuthService } from '../../services/auth/auth-service';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
+
 export class NavbarComponent {
+
+  constructor(
+    private authService: AuthService
+  ) {}
+
+  get isLogged() {
+    return this.authService.isLogged();
+  }
 
 }
