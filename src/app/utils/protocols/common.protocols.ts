c0d6000dd1398c7a406959a9e59f9699d92395ed
Arz Dev <arz.dev@outlook.com>
2024-06-09 11:09:11 -0600

    [utils/protocols/common] + create account

--- src/app/utils/protocols/common.protocols.ts
+++ src/app/utils/protocols/common.protocols.ts
@@ -3,3 +3,4 @@ export const EMPTY : string = '';
 export const LOGIN : string = 'login';
 export const WELCOME : string = 'welcome';
 export const ESCOMIO: string = 'chat';
+export const CREATE_ACCOUNT: string = 'create';
