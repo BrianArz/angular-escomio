7dcbcdb78bfc56c2aa49e5119ede842c9a4625ef
Arz Dev <arz.dev@outlook.com>
2024-06-08 18:55:26 -0600

    [component/navbar/logic] + refactor

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { AuthService } from '../../services/auth/auth-service';
+
+import * as APP from '../../utils/protocols/common.protocols';
 
 @Component({
   selector: 'app-navbar',
@@ -14,9 +16,17 @@ import { AuthService } from '../../services/auth/auth-service';
 
 export class NavbarComponent {
 
+  chatRoute: string = APP.ESCOMIO;
+
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate([APP.WELCOME]);
+  }
 
   get isLogged() {
     return this.authService.isLogged();
