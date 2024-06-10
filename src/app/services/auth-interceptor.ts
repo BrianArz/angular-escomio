156cedebacf5526e90b7d12ca19387a966a30d4e
Arz Dev <arz.dev@outlook.com>
2024-06-10 00:25:06 -0600

    [services/auth-interceptor] + refactor to function

--- src/app/services/auth-interceptor.ts
+++ src/app/services/auth-interceptor.ts
@@ -1,46 +1,42 @@
+import { HttpInterceptorFn, HttpRequest, HttpHandlerFn, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { AuthService } from './auth/auth-service';
+import { Router } from '@angular/router';
 
-import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import * as ROUTES from '../utils/protocols/common.protocols';
 
+export const AuthInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn): Observable<any> => {
+    const authService = inject(AuthService);
+    const router = inject(Router);
 
-@Injectable({
-    providedIn: 'root'
-})
-export class AuthInterceptor implements HttpInterceptor {
+    // Clone the request to add the new header.
+    let headers = new HttpHeaders();
 
-    constructor(
-    ) { }
-
-    /**
-     * Adds auth cookie to every request
-     */
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-        // Clone the request to add the new header.
-        let headers = new HttpHeaders();
-
-
-        // add previously added headers
-        const keys = req.headers.keys();
-        if (keys.length > 0) {
-            keys.forEach(x => {
-                headers = headers.append(x, req.headers.get(x) ?? '');
-            })
-        }
-
-        const authReq = req.clone({ headers: headers, withCredentials: true });
-
-        /** 
-         * If response is unauthorized, redirect to login
-         */
-        return next.handle(authReq).pipe( tap(() => {},
-        (err: any) => {
-        if (err instanceof HttpErrorResponse) {
-          if (err.status !== 401) {
-           return;
-          }
-        }
-      }));
+    // Add previously added headers
+    const keys = req.headers.keys();
+    if (keys.length > 0) {
+        keys.forEach(x => {
+            headers = headers.append(x, req.headers.get(x) ?? '');
+        });
     }
-}
+
+    const authReq = req.clone({ headers: headers, withCredentials: true });
+
+    /** 
+     * If response is unauthorized, redirect to login
+     */
+    return next(authReq).pipe(
+        tap(() => { },
+            (err: any) => {
+                if (err instanceof HttpErrorResponse) {
+                    if (err.status !== 401) {
+                        return;
+                    }
+                    authService.clearEscomioLocalStorage();
+                    router.navigate([ROUTES.LOGIN]);
+                }
+            })
+    );
+};
