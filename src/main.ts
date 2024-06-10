7c8344c3b2f55fca7888350959e70e6e1836cd75
Arz Dev <arz.dev@outlook.com>
2024-06-10 00:24:45 -0600

    [src/main.ts] + interceptor logic

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { AuthInterceptor } from './app/services/auth-interceptor';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([AuthInterceptor])
+    )
+  ]
+}).catch(err => console.error(err));
