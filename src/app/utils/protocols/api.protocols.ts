d79431118afbdfb6d3dcf626ac09c15657f5b7e1
Arz Dev <arz.dev@outlook.com>
2024-06-16 23:33:29 -0600

    [protocols/api] + update message grade + suggest by chat

--- src/app/utils/protocols/api.protocols.ts
+++ src/app/utils/protocols/api.protocols.ts
@@ -16,6 +16,8 @@ export const GET_CONVERSATIONS: string = 'rasa/get-conversations';
 export const GET_CONVERSATIONS_MESSAGES: string = 'rasa/get-conversation_messages';
 export const CREATE_CONVERSATION: string = 'rasa/create-conversation';
 export const ADD_MESSAGE: string = 'rasa/add-message-to-conversation';
+export const UPDATE_MESSAGE_GRADE: string = 'rasa/update-message-grade';
+export const SUGGEST_BY_CHAT: string = 'rasa/suggest-by-chat';
 
 // API Health Endpoints
 export const GET_SERVICE_VERSION: string = "health/get-version";
