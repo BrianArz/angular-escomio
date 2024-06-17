846371dd03caeb14f92919127c8930864ec75c24
Arz Dev <arz.dev@outlook.com>
2024-06-16 23:34:12 -0600

    [service/rasa] + sugggest by chat + update message grade

--- src/app/services/rasa/rasa-service.ts
+++ src/app/services/rasa/rasa-service.ts
@@ -11,9 +11,11 @@ import { ConversationIdRequest } from '../../models/rasa/conversation-id-request
 import { QuestionRequest } from '../../models/rasa/question-request';
 import { CreateConversationResponse } from '../../models/rasa/create-conversation-response';
 import { AddMessageRequest } from '../../models/rasa/add-message-request';
+import { UpdateMessageGradeRequest } from '../../models/rasa/update-message-grade-request';
+import { ApiMessageResponse } from '../../models/api/api-message-response';
+import { SuggestByChatRequest } from '../../models/rasa/suggest-by-chat-request';
 
 import * as API from '../../utils/protocols/api.protocols';
-
 
 @Injectable({
     providedIn: 'root'
@@ -53,6 +55,22 @@ export class RasaService {
     public addMessage(addMessageRequest: AddMessageRequest): Observable<CreateConversationResponse> {
         const url = this.formApiUrl(API.ADD_MESSAGE);
         return this.http.put<CreateConversationResponse>(url, addMessageRequest, { withCredentials: true }).pipe(
+            map(response => response),
+            catchError(error => HttpErrorHandler.handleHttpError(error))
+        )
+    }
+
+    public updateMessageGrade(updateMessageGradeRequest: UpdateMessageGradeRequest): Observable<ApiMessageResponse> {
+        const url = this.formApiUrl(API.UPDATE_MESSAGE_GRADE);
+        return this.http.put<ApiMessageResponse>(url, updateMessageGradeRequest, { withCredentials: true }).pipe(
+            map(response => response),
+            catchError(error => HttpErrorHandler.handleHttpError(error))
+        )
+    }
+
+    public suggestByChat(suggetByChatRequest: SuggestByChatRequest): Observable<ApiMessageResponse> {
+        const url = this.formApiUrl(API.SUGGEST_BY_CHAT);
+        return this.http.post<ApiMessageResponse>(url, suggetByChatRequest, { withCredentials: true }).pipe(
             map(response => response),
             catchError(error => HttpErrorHandler.handleHttpError(error))
         )
